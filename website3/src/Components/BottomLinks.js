8974a7a55de8a3f1aed5772d9a1ddd7a695dd17e
Andre Fu <andrefu.af@hotmail.com>
2023-07-21 19:25:07 -0700

    .

--- website3/src/Components/BottomLinks.js
+++ website3/src/Components/BottomLinks.js
@@ -4,10 +4,10 @@ import '../App.css'; // Assuming you have a CSS file named App.css
 const BottomLinks = () => {
   return (
     <div className="bottom-links">
-      <a href="/resume" className="link">RESUME.</a>
-      <a href="https://www.linkedin.com" className="link">LINKEDIN.</a>
-      <a href="https://github.com" className="link">GITHUB.</a>
-      <a href="/bookshelf" className="link">BOOKSHELF.</a>
+      <a href="../../resume.pdf" className="link">RESUME.</a>
+      <a href="https://www.linkedin.com/in/andre-fu/" className="link">LINKEDIN.</a>
+      <a href="https://github.com/andre-fu/" className="link">GITHUB.</a>
+      <a href="/bookshelf" className="link">BLOG.</a>
     </div>
   );
 };
